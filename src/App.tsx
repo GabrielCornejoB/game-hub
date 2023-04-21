1e39152c0bc300fdee192889171bfdf34f13214c
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-21 14:37:30 -0500

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Hola mundo</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"',
+        lg: '"nav nav" "aside main"', // Wider tan 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerBlue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
