0c4e4f721b9fc7a41882ca2c5127b032a14ebe72
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-22 12:20:08 -0500

    Refactor: Query object

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,11 @@
 import useData from "./useData";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () => useData<Platform>("/platforms/lists/parents");
 
-export default usePlatforms;
+export default usePlatform;
