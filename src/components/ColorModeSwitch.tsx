336c1f0dbd5ca77919a7f495a0a0e3806229047e
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-22 12:52:01 -0500

    Search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ function ColorModeSwitch() {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark mode</Text>
+      <Text whiteSpace="nowrap">Dark mode</Text>
     </HStack>
   );
 }
