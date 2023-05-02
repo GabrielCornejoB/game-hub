86ec5c5766853d78551fd4bd0b4e6975bbce78f2
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-01 23:18:24 -0500

    Refreshed API KEY and changed to env

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "5fcd333a5e954b8aba353efc27d0bc74",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
